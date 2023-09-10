2f57d8fe73b41a6c4329b7040305cbff2851e05c
Anshul Bhatt <134294749+Anshul123B@users.noreply.github.com>
2023-09-10 20:04:37 +0530

    Add files via upload

---
+++ swapwithout3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int a , b;
+	printf("Enter the value of a: ");
+	scanf("%d", &a);
+	printf("Enter the value of b: ");
+	scanf("%d", &b);
+	
+	a = a+b;
+	b = a-b;
+	a = a-b;
+	
+	printf("Value of a after swaping: %d\n", a);
+	printf("Value of b after swaping: %d", b);
+	
+	return 0;
+}
