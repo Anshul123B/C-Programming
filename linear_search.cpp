1194cd611f9ab7c3a8356743ebbbbf7330a21cd4
Anshul Bhatt <134294749+Anshul123B@users.noreply.github.com>
2023-11-23 21:58:02 +0530

    Update linear_search.cpp

--- linear_search.cpp
+++ linear_search.cpp
@@ -1,40 +1,29 @@
 #include <stdio.h>
-
-// Function to perform linear search
 int linearSearch(int arr[], int n, int target) {
     for (int i = 0; i < n; i++) {
         if (arr[i] == target) {
-            return i; // Return the index if target is found
+            return i; 
         }
     }
-    return -1; // Return -1 if target is not found in the array
+    return -1; 
 }
 
 int main() {
     int n;
-
-    // Get the size of the array from the user
     printf("Enter the size of the array: ");
     scanf("%d", &n);
-
+    
     int arr[n];
-
-    // Get array elements from the user
     printf("Enter %d elements:\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
     int target;
-
-    // Get the target value to search for
     printf("Enter the target value to search for: ");
     scanf("%d", &target);
-
-    // Perform linear search
     int result = linearSearch(arr, n, target);
 
-    // Display the result
     if (result != -1) {
         printf("Target value %d found at index %d.\n", target, result);
     } else {
